358929920494e313f7296299f57f9b23891789c1
Natale Patriciello <natale.patriciello@gmail.com>
2017-09-03 17:03:20 +0200

    tcp: added get_segs_per_round
    Usually, the pacing time is provided per-segment. In some occasion, this
    time refers to the time between a group of segments. With this commit, add
    the possibility for the congestion control module to tell the TCP socket
    how many segments can be sent out before pausing and setting a pacing
    timer.
    
    Signed-off-by: Natale Patriciello <natale.patriciello@gmail.com>
    Acked-by: Carlo Augusto Grazia <carloaugusto.grazia@unimore.it>

--- net/ipv4/tcp_output.c
+++ net/ipv4/tcp_output.c
@@ -2249,6 +2249,7 @@ static bool tcp_write_xmit(struct sock *sk, unsigned int mss_now, int nonagle,
 	int result;
 	bool is_cwnd_limited = false, is_rwnd_limited = false;
 	u32 max_segs;
+	u32 pacing_allowed_segs = 0;
 
 	sent_pkts = 0;
 
@@ -2266,14 +2267,18 @@ static bool tcp_write_xmit(struct sock *sk, unsigned int mss_now, int nonagle,
 	tcp_mstamp_refresh(tp);
 
 	if (!tcp_pacing_timer_check(sk)) {
+		pacing_allowed_segs = 1;
 		if (ca_ops->pacing_timer_expired)
 			ca_ops->pacing_timer_expired(sk);
+		if (ca_ops->get_segs_per_round)
+			pacing_allowed_segs = ca_ops->get_segs_per_round(sk);
 	}
 
 	while ((skb = tcp_send_head(sk))) {
 		unsigned int limit;
 
-		if (tcp_pacing_check(sk))
+		if (tcp_needs_internal_pacing(sk) &&
+		    sent_pkts >= pacing_allowed_segs)
 			break;
 
 		tso_segs = tcp_init_tso_segs(skb, mss_now);
